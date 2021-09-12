15917c696947dd1a67a98594911ebf44f434e4c3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-12 17:51:57 +0200

    get current day name and render list ejs template with this day name

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express")
+const ejs = require("ejs")
 const port = 3000 
 
 const app = express()
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html")
+    let currentDate = new Date()
+    let currentDayNumber = currentDate.getDay()
+    let currentDayName = ""
+    console.log(`current date is : ${currentDate}`)
+
+    switch(currentDayNumber) {
+        case 0: currentDayName = "dimanche"; break
+        case 1: currentDayName = "lundi"; break
+        case 2: currentDayName = "mardi"; break
+        case 3: currentDayName = "mercredi"; break
+        case 4: currentDayName = "jeudi"; break
+        case 5: currentDayName = "vendredi"; break
+        case 6: currentDayName = "samedi"; break
+        default: currentDayName = "N/A"; console.log(`error... current day is number ${currentDayNumber} `)
+    }
+    res.render('list', {currentDayName : currentDayName})
 })
 
 app.listen(port, function() {
