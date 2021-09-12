fd5463455d41569b3dfece2e1a7ba04f30d2c52d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-12 16:48:49 +0200

    Initial commit with starting files & minimal configuration

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const port = 3000 
+
+const app = express()
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(port, function() {
+    console.log(`Server started on port ${port}`)
+})
