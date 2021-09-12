850f4e5310c1011122fba58e59f72507f400dec3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-12 20:26:24 +0200

    display list of tasks into list.ejs and add method to add new task

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const ejs = require("ejs")
 const port = 3000 
 
 const app = express()
+app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 
+let tasksList = ["Faire du sport", "Coder du javascript", "Dessiner 1/4 d'heure"]
+
 app.get("/", function(req, res) {
-    
     let currentDate = new Date()
     let options = {weekday: "long", year: "numeric", month: "long", day: "numeric"}
     let currentDateFormatted = currentDate.toLocaleDateString("fr-FR", options)
-    res.render('list', {currentDateFormatted : currentDateFormatted})
+    res.render('list', {currentDateFormatted : currentDateFormatted, tasksList : tasksList})
+})
+
+app.post("/", function(req, res) {
+    console.log("request on '/' url")
+    const newTask = req.body.newTask
+    console.log(`new task = ${newTask}`)
+    tasksList.push(newTask)
+    console.log("task list = " + tasksList)
+    res.redirect("/")
 })
 
 app.listen(port, function() {
