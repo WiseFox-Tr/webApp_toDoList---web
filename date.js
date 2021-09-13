63be1d0ef5b0078d4bb97d306d57dc7572ec742c
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-13 19:26:58 +0200

    delete useless semi colon

--- date.js
+++ date.js
@@ -2,4 +2,4 @@ exports.getCurrentDate = function() {
     const currentDate = new Date()
     const options = {weekday: "long", year: "numeric", month: "long", day: "numeric"}
     return currentDate.toLocaleDateString("fr-FR", options)
-};
+}
