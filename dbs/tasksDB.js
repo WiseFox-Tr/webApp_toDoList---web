53e9e9d13cef522204efe96961e0c8111d2cc24f
Maxime Traverse <maxime.Traverse@orange.fr>
2021-10-05 19:28:10 +0200

    remove common methods connect & disconnect

--- dbs/tasksDB.js
+++ dbs/tasksDB.js
@@ -1,14 +1,3 @@
-const mongoose = require("mongoose")
-
-exports.connectToDB = function(){
-    return mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`)
-}
-
-exports.disconnectToDB = function(){
-    mongoose.connection.close()
-}
-
-
 const itemSchema = mongoose.Schema({
     name: {
         type: String,
