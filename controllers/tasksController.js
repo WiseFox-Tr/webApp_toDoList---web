16b343014d71a182c505a21f43eef5f4d96db5d0
Maxime Traverse <maxime.Traverse@orange.fr>
2021-10-05 19:19:51 +0200

    update calls to connect & disconnect db functions

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,41 +1,42 @@
+const commonDB = require("../dbs/commonDB")
 const tasksDB = require("../dbs/tasksDB.js")
 
 exports.displayTasks = async function(res) {
     try {
-        await tasksDB.connectToDB()
+        await commonDB.connectToDB()
         const tasksItems = await tasksDB.getTaskItems()
         res.render('list', {listTitle : "Vos tâches", tasksList : tasksItems})
     } catch(e) {
         console.log(`Error - unable to load tasks list ! \n${e}`)
         res.send("Unable to load tasks list")
     } finally {
-        tasksDB.disconnectToDB()
+        commonDB.disconnectToDB()
     }
 }
 
 exports.addNewTask = async function(newTask, res) {
     try {
         if(!newTask) throw Error("Error task name field is empty")
-        await tasksDB.connectToDB()
+        await commonDB.connectToDB()
         await tasksDB.addNewTask(newTask.trim())
         console.log(`task ${newTask} successfully added`)
     } catch(e) {
         console.log(`Error - adding new task failled ! \n${e}`)
     } finally {
-        tasksDB.disconnectToDB()
+        commonDB.disconnectToDB()
         res.redirect("/tasks")
     }
 }
 
 exports.deleteTaskById = async function(taskId, res) {
     try {
-        await tasksDB.connectToDB()
+        await commonDB.connectToDB()
         await tasksDB.deleteTaskById(taskId)
         console.log(`Deleting task with id ${taskId} succeed !`)
     } catch(e) {
         console.log(`Deleting item with id ${taskId} failled ! \n${e}`)
     } finally {
-        tasksDB.disconnectToDB()
+        commonDB.disconnectToDB()
         res.redirect("/tasks")
     }
 }
