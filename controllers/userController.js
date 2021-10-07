d9237dae8744ba7b4acd0e36da63c5b6560f2bc2
Maxime Traverse <maxime.Traverse@orange.fr>
2021-10-07 22:34:31 +0200

    put authenticateUser method into userController + add a getUserById method in it & use them into passportConfig !

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,56 @@
 const bcrypt = require("bcrypt")
-
+const passport = require("passport")
 const commonDB = require("../dbs/commonDB.js")
 const userDB = require("../dbs/userDB.js")
-
 
 async function registerUser (req, res) {
     try {
         await commonDB.connectToDB()
+        console.log(`plain info mail : ${req.body.email} & pwd : ${req.body.password}`)
         await userDB.saveUser(req.body.email, await bcrypt.hash(req.body.password, 10))
         res.redirect("/login")
     } catch(e) {
         console.log(`register error : ${e}`)
         res.redirect("/register")
-
     } finally {
         commonDB.disconnectToDB()
     }
 }
 
+//login method
+async function authenticateUser(email, password, done) {
+    try {            
+        await commonDB.connectToDB()
+        const user = await userDB.getUserByEmail(email)
+
+        if(!user) {
+            return done(null, false, {message: `Aucun utilisateur pour le mail ${email}`})
+        }
+        if(await bcrypt.compare(password, user.password)) {
+            return done(null, user)
+        } else {
+            return done(null, false, {message: "Le mot de passe saisi ne correspond pas à celui de ce compte"})
+        }
+    } catch(e) {
+        console.log(`log in error --> ${e}`)
+        return done(null, false, {message: "Oups .. un problème est survenu !"})
+    } finally {
+        commonDB.disconnectToDB()
+    }
+}
+
+async function getUserById(id) {
+    try {
+        await commonDB.connectToDB()
+        await userDB.getUserById(id)
+    } catch(e) {
+        console.log(`getUserById error --> ${e}`)
+    }
+    commonDB.disconnectToDB()
+}
+
 module.exports = {
     registerUser,
+    authenticateUser, 
+    getUserById
 }
--- services/passportConfig.js
+++ services/passportConfig.js
@@ -1,39 +1,23 @@
 const LocalStrategy = require("passport-local").Strategy
-const bcrypt = require("bcrypt")
-const commonDB = require("../dbs/commonDB.js")
-const userDB = require("../dbs/userDB.js")
+const userController = require("../controllers/userController.js")
 
+//take instance of passport
+function initialize(passport) {
 
-//take instance of passport & function getUserById method
-function initialize(passport, getUserById) {
-
-    //make sure user email & password correct by calling authenticateUser method (inner function)
+    //make sure user email & password are corrects by calling authenticateUser method from userController
     const authenticateUser = async (email, password, done) => {
-        try {            
-            await commonDB.connectToDB()
-            const user = await userDB.getUserByEmail(email)
-            //if user null, return null error & false to say it did not succeed to find user + message
-            if(!user) {
-                return done(null, false, {message: "No user with that email"})
-            }
-            //if passwords match -> return done with user 
-            if(await bcrypt.compare(password, user.password)) {
-                return done(null, user)
-            } else {
-                return done(null, false, {message: "Password provided doesn't match with user password"})
-            }
-        } catch(e) {
-            return done(e, false, {message: "oups a problem occurs"})
-        } finally {
-            commonDB.disconnectToDB()
-        }
+        await userController.authenticateUser(email, password, done)
     }
     
     //define LocalStrategy
-    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    passport.use(new LocalStrategy(
+        {usernameField: 'email'},
+        authenticateUser
+    ))
+
     //serialize & deserialize user
     passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => done(null, getUserById(id)))
+    passport.deserializeUser((id, done) => done(null, async () => userController.getUserById(id)))
 }
 
 module.exports = initialize
